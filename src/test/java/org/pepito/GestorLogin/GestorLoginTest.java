0548f95512864b3fcd6d8596d52c420cd2aaec72
Carlos Ramos <CarlosRamos@MacBook-Pro-de-T0reto.local>
2017-11-07 17:21:39 +0100

    Test JUnit

---
+++ src/test/java/org/pepito/GestorLogin/GestorLoginTest.java
@@ -0,0 +1,21 @@
+package org.pepito.GestorLogin;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GestorLoginTest {
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @After
+  public void tearDown() throws Exception {}
+
+  @Test
+  public void test() {
+    
+  }
+
+}
