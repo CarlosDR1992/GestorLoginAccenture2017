e0c734eecead48bae12fcddac3bc798732fdddc1
Carlos Ramos <CarlosRamos@MacBook-Pro-de-T0reto.local>
2017-11-07 17:37:08 +0100

    First test

--- src/test/java/org/pepito/GestorLogin/GestorLoginTest.java
+++ src/test/java/org/pepito/GestorLogin/GestorLoginTest.java
@@ -1,21 +1,48 @@
 package org.pepito.GestorLogin;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class GestorLoginTest {
-
+  GestorLogin login;
+  IRepositorioCuentas repo;
+  ICuenta cuenta;
+  
   @Before
-  public void setUp() throws Exception {}
-
-  @After
-  public void tearDown() throws Exception {}
-
-  @Test
-  public void test() {
-    
+  public void setUp() throws Exception {
+    repo = mock(IRepositorioCuentas.class);
+    cuenta = mock(ICuenta.class);
+    when(repo.buscar("pepe")).thenReturn(cuenta);
+    login = new GestorLogin(repo);
   }
 
+  @After
+  public void tearDown() throws Exception {
+    repo = null;
+    cuenta = null;
+    login = null;
+  }
+
+  @Test
+  public void testAccesoConcedidoALaPrimera() {
+    when(cuenta.claveCorrecta("1234")).thenReturn(true);
+    
+    login.acceder("pepe", "1234");
+    
+    verify(cuenta, times(1)).entrarCuenta();
+    verify(cuenta, never()).bloquearCuenta();
+  }
+  
+  @Test
+  public void testSeDeniegaAccesoALaPrimeraVez() {
+    when(cuenta.claveCorrecta("1234")).thenReturn(true);
+    
+    login.acceder("pepe", "1234");
+    
+    verify(cuenta, never()).bloquearCuenta();
+  }
+  
 }
