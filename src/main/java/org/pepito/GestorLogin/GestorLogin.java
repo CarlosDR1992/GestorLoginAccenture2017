d4225ac6f01f885c523d7744c532e653620abacc
Carlos Ramos <CarlosRamos@MacBook-Pro-de-T0reto.local>
2017-11-07 19:20:15 +0100

    More Tests

--- src/main/java/org/pepito/GestorLogin/GestorLogin.java
+++ src/main/java/org/pepito/GestorLogin/GestorLogin.java
@@ -3,41 +3,44 @@ package org.pepito.GestorLogin;
 
 public class GestorLogin {
 
-	private IRepositorioCuentas repo;
-	private int numFallos;
-	private static final int MAX_FALLOS = 3;
-	private String usuarioAnterior = "";
+  private IRepositorioCuentas repo;
+  private int numFallos;
+  private static final int MAX_FALLOS = 3;
+  private String usuarioAnterior = "";
 
-	public GestorLogin(IRepositorioCuentas repo) {
-		this.repo = repo;
-		numFallos = 0;
-	}
+  public GestorLogin(IRepositorioCuentas repo) {
+    this.repo = repo;
+    numFallos = 0;
+  }
 
-	public void acceder(String usuario, String clave) {
-		ICuenta cuenta = repo.buscar(usuario);
-		
-		if (cuenta == null)
-			throw new ExcepcionUsuarioDesconocido();
-		
-		if (cuenta.estaEnUso())
-			throw new ExcepcionCuentaEnUso();
+  public int getNumFallos() {
+    return numFallos;
+  }
 
-		if (! cuenta.estaBloqueada()) {
+  public void acceder(String usuario, String clave) {
+    ICuenta cuenta = repo.buscar(usuario);
 
-			if (cuenta.claveCorrecta(clave)) {
-				cuenta.entrarCuenta();
-			}
-			else {
-				if (! usuario.equals(usuarioAnterior)) {
-					numFallos = 0;
-					usuarioAnterior = usuario;
-				}
-				numFallos++;
-				if (numFallos == MAX_FALLOS) {
-					cuenta.bloquearCuenta();
-				}
-			}
-		}
-	}
+    if (cuenta == null)
+      throw new ExcepcionUsuarioDesconocido();
+
+    if (cuenta.estaEnUso())
+      throw new ExcepcionCuentaEnUso();
+
+    if (!cuenta.estaBloqueada()) {
+
+      if (cuenta.claveCorrecta(clave)) {
+        cuenta.entrarCuenta();
+      } else {
+        if (!usuario.equals(usuarioAnterior)) {
+          numFallos = 0;
+          usuarioAnterior = usuario;
+        }
+        numFallos++;
+        if (numFallos == MAX_FALLOS) {
+          cuenta.bloquearCuenta();
+        }
+      }
+    }
+  }
 
 }
--- src/test/java/org/pepito/GestorLogin/GestorLoginTest.java
+++ src/test/java/org/pepito/GestorLogin/GestorLoginTest.java
@@ -5,12 +5,13 @@ import static org.mockito.Mockito.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.hamcrest.Matchers.*;
 
 public class GestorLoginTest {
   GestorLogin login;
   IRepositorioCuentas repo;
   ICuenta cuenta;
-  
+
   @Before
   public void setUp() throws Exception {
     repo = mock(IRepositorioCuentas.class);
@@ -29,20 +30,125 @@ public class GestorLoginTest {
   @Test
   public void testAccesoConcedidoALaPrimera() {
     when(cuenta.claveCorrecta("1234")).thenReturn(true);
-    
+
     login.acceder("pepe", "1234");
-    
+
+    verify(cuenta, times(1)).entrarCuenta();
+    verify(cuenta, never()).bloquearCuenta();
+  }
+
+  @Test
+  public void testSeDeniegaAccesoALaPrimeraVez() {
+    when(cuenta.claveCorrecta("12345")).thenReturn(false);
+
+    login.acceder("pepe", "12345");
+
+    verify(cuenta, never()).entrarCuenta();
+  }
+
+  @Test
+  public void testUsuarioDesconocido() {
+    when(repo.buscar("manolo")).thenThrow(ExcepcionUsuarioDesconocido.class);
+
+    try {
+      login.acceder("manolo", anyString());
+      fail("Debe lanzar una excepcion");
+    } catch (ExcepcionUsuarioDesconocido e) {
+      verify(repo).buscar("manolo");
+    }
+
+
+  }
+
+  @Test
+  public void siFallaPrimaVez_AccesoNoConcedido_NoBloquear() {
+    when(cuenta.claveCorrecta("12345")).thenReturn(false);
+
+    login.acceder("pepe", "12345");
+
+    verify(cuenta, never()).entrarCuenta();
+    verify(cuenta, never()).bloquearCuenta();
+    assertThat(login.getNumFallos(), is(1));
+  }
+
+  @Test
+  public void seBloqueaCuentaDespuesTresIntentos() {
+    when(cuenta.claveCorrecta("12345")).thenReturn(false);
+
+    login.acceder("pepe", "12345");
+    login.acceder("pepe", "12345");
+    login.acceder("pepe", "12345");
+
+    assertThat(login.getNumFallos(), is(3));
+    verify(cuenta, times(1)).bloquearCuenta();
+  }
+
+  @Test
+  public void sePuedeAccederTrasUnFallo() {
+    when(cuenta.claveCorrecta("12345")).thenReturn(false);
+
+    login.acceder("pepe", "12345");
+
+    assertThat(login.getNumFallos(), is(1));
+    verify(cuenta, never()).entrarCuenta();
+  }
+
+  @Test
+  public void sePuedeAccederTrasDosFallos() {
+    when(cuenta.claveCorrecta("12345")).thenReturn(false);
+
+    login.acceder("pepe", "12345");
+    login.acceder("pepe", "12345");
+
+    assertThat(login.getNumFallos(), is(2));
+    verify(cuenta, never()).entrarCuenta();
+  }
+
+  @Test
+  public void seBloqueaLaCuentaDespuesCuatroIntentos() {
+    when(cuenta.claveCorrecta("12345")).thenReturn(false);
+
+    login.acceder("pepe", "12345");
+    login.acceder("pepe", "12345");
+    login.acceder("pepe", "12345");
+    login.acceder("pepe", "12345");
+
+    assertThat(login.getNumFallos(), is(4));
+    verify(cuenta, times(1)).bloquearCuenta();
+  }
+
+  @Test
+  public void otroUsuarioPuedeAccederTrasBloqueo() {
+    when(cuenta.claveCorrecta("12345")).thenReturn(false);
+
+    login.acceder("pepe", "12345");
+
+    assertThat(login.getNumFallos(), is(1));
+    verify(cuenta).bloquearCuenta();
+
+    login.acceder("carlos", "12345");
+
     verify(cuenta, times(1)).entrarCuenta();
     verify(cuenta, never()).bloquearCuenta();
   }
   
   @Test
-  public void testSeDeniegaAccesoALaPrimeraVez() {
-    when(cuenta.claveCorrecta("1234")).thenReturn(true);
+  public void siEstaBloqueada_SeDeniegaAcceso() {
+    when(cuenta.estaBloqueada()).thenReturn(true);
     
-    login.acceder("pepe", "1234");
+    login.acceder("pepe", anyString());
     
-    verify(cuenta, never()).bloquearCuenta();
+    verify(cuenta, never()).entrarCuenta();
   }
-  
+
+  @Test(expected = ExcepcionCuentaEnUso.class)
+  public void seDeniegaAccesoCuentasEnUso() {
+    when(cuenta.claveCorrecta("12345")).thenReturn(true);
+
+    verify(cuenta).estaEnUso();
+
+    login.acceder("pepe", "12345");
+
+
+  }
 }
